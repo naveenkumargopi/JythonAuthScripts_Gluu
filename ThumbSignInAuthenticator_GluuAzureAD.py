5c2ad3cc9204a01fe257ce6d781e08d808278b2a
Naveen Kumar G <naveen.gopi@imaginea.com>
2018-11-21 19:59:22 +0530

    Added TSI script for Gluu with Azure AD

--- ThumbSignInAuthenticator_GluuAzureAD.py
+++ ThumbSignInAuthenticator_GluuAzureAD.py
@@ -3,15 +3,19 @@
 from org.xdi.service.cdi.util import CdiUtil
 from org.xdi.oxauth.security import Identity
 from org.xdi.model.custom.script.type.auth import PersonAuthenticationType
-from org.xdi.oxauth.service import AuthenticationService
-from org.xdi.util import StringHelper
+from org.xdi.oxauth.service import AuthenticationService, UserService
+from org.xdi.oxauth.model.common import User
+from org.xdi.util import StringHelper, ArrayHelper
 from org.xdi.oxauth.util import ServerUtil
 from com.pramati.ts.thumbsignin_java_sdk import ThumbsigninApiController
+from com.thumbsignin import AzureAuthConnector
 from org.json import JSONObject
 from org.xdi.oxauth.model.util import Base64Util
 from java.lang import String
+from java.util import Arrays, ArrayList, HashMap, IdentityHashMap
 
 import java
+
 
 class PersonAuthentication(PersonAuthenticationType):
 
@@ -35,8 +39,69 @@ class PersonAuthentication(PersonAuthenticationType):
         global ts_statusPath
         ts_statusPath = "/ts/secure/txn-status/"
 
+        global azure_tenant_id
+        azure_tenant_id = configurationAttributes.get("azure_tenant_id").getValue2()
+        print "ThumbSignIn. Initialization. Value of azure_tenant_id is %s" % azure_tenant_id
+
+        global azure_client_id
+        azure_client_id = configurationAttributes.get("azure_client_id").getValue2()
+        print "ThumbSignIn. Initialization. Value of azure_client_id is %s" % azure_client_id
+
+        global azure_client_secret
+        azure_client_secret = configurationAttributes.get("azure_client_secret").getValue2()
+        print "ThumbSignIn. Initialization. Value of azure_client_secret is %s" % azure_client_secret
+
+        self.azureAuthConnector = AzureAuthConnector()
+
+        if (configurationAttributes.containsKey("azuread_attributes_list") and
+                configurationAttributes.containsKey("gluuldap_attributes_list")):
+
+            azureadAttributesList = configurationAttributes.get("azuread_attributes_list").getValue2()
+            if (StringHelper.isEmpty(azureadAttributesList)):
+                print "ThumbSignIn: Initialization. The property azuread_attributes_list is empty"
+                return False
+
+            gluuldapAttributesList = configurationAttributes.get("gluuldap_attributes_list").getValue2()
+            if (StringHelper.isEmpty(gluuldapAttributesList)):
+                print "ThumbSignIn: Initialization. The property gluuldap_attributes_list is empty"
+                return False
+
+            self.attributesMapping = self.prepareAttributesMapping(azureadAttributesList, gluuldapAttributesList)
+            if (self.attributesMapping == None):
+                print "ThumbSignIn: Initialization. The attributes mapping isn't valid"
+                return False
+
         print "ThumbSignIn. Initialized successfully"
         return True
+
+    def prepareAttributesMapping(self, azureadAttributesList, gluuldapAttributesList):
+        try:
+            azureadAttributesListArray = StringHelper.split(azureadAttributesList, ",")
+            if (ArrayHelper.isEmpty(azureadAttributesListArray)):
+                print("ThumbSignIn: PrepareAttributesMapping. There is no attributes specified in azureadAttributesList property")
+                return None
+
+            gluuldapAttributesListArray = StringHelper.split(gluuldapAttributesList, ",")
+            if (ArrayHelper.isEmpty(gluuldapAttributesListArray)):
+                print("ThumbSignIn: PrepareAttributesMapping. There is no attributes specified in gluuldapAttributesList property")
+                return None
+
+            if (len(azureadAttributesListArray) != len(gluuldapAttributesListArray)):
+                print("ThumbSignIn: PrepareAttributesMapping. The number of attributes in azureadAttributesList and gluuldapAttributesList isn't equal")
+                return None
+
+            attributeMapping = IdentityHashMap()
+            i = 0
+            count = len(azureadAttributesListArray)
+            while (i < count):
+                azureadAttribute = StringHelper.toLowerCase(azureadAttributesListArray[i])
+                gluuldapAttribute = StringHelper.toLowerCase(gluuldapAttributesListArray[i])
+                attributeMapping.put(azureadAttribute, gluuldapAttribute)
+                i = i + 1
+
+            return attributeMapping
+        except Exception, err:
+            print("ThumbSignIn: Exception inside prepareAttributesMapping " + str(err))
 
     def destroy(self, configurationAttributes):
         print "ThumbSignIn. Destroy"
@@ -198,92 +263,175 @@ class PersonAuthentication(PersonAuthenticationType):
             login_flow = ServerUtil.getFirstValue(requestParameters, "login_flow")
             print "ThumbSignIn. Value of login_flow parameter is %s" % login_flow
 
-            #Logic for ThumbSignIn Authentication Flow
+            # Logic for ThumbSignIn Authentication Flow (Either step 1 or step 3)
             if (login_flow == "ThumbSignIn_Authentication" or login_flow == "ThumbSignIn_RegistrationSucess"):
                 identity.setWorkingParameter("userLoginFlow", login_flow)
                 print "ThumbSignIn. Value of userLoginFlow is %s" % identity.getWorkingParameter("userLoginFlow")
-
-                transactionId = ServerUtil.getFirstValue(requestParameters, "transactionId")
-                print "ThumbSignIn. Value of transactionId is %s" % transactionId
-                getUserRequest = "getUser/" + transactionId
-                print "ThumbSignIn. Value of getUserRequest is %s" % getUserRequest
-
-                getUserResponseJsonStr = thumbsigninApiController.handleThumbSigninRequest(getUserRequest, ts_apiKey, ts_apiSecret)
-                print "ThumbSignIn. Value of getUserResponseJsonStr is %s" % getUserResponseJsonStr
-                getUserResponseJsonObj = JSONObject(getUserResponseJsonStr)
-                thumbSignIn_UserId = getUserResponseJsonObj.get("userId")
-                print "ThumbSignIn. Value of thumbSignIn_UserId is %s" % thumbSignIn_UserId
-
-                logged_in_status = authenticationService.authenticate(thumbSignIn_UserId)
+                logged_in_status = authenticationService.authenticate(self.getUserIdFromThumbSignIn(requestParameters, thumbsigninApiController))
                 print "ThumbSignIn. logged_in status : %r" % (logged_in_status)
                 return logged_in_status
 
-            #Logic for ThumbSignIn Registration Flow
+            # Logic for username/password login flow (step 1)
+            print "ThumbSignIn. User credentials login flow"
             identity.setWorkingParameter("userLoginFlow", "ThumbSignIn_Registration")
             print "ThumbSignIn. Value of userLoginFlow is %s" % identity.getWorkingParameter("userLoginFlow")
-            credentials = identity.getCredentials()
+            logged_in = self.authenticateUserInAzureAD(identity, authenticationService)
+            print "ThumbSignIn. Status of User Credentials based Authentication : %r" % (logged_in)
 
-            user_name = credentials.getUsername()
-            user_password = credentials.getPassword()
-            print "ThumbSignIn. user_name: " + user_name
-            #print "ThumbSignIn. user_password: " + user_password
-
-            logged_in = False
-            if (StringHelper.isNotEmptyString(user_name) and StringHelper.isNotEmptyString(user_password)):
-                logged_in = authenticationService.authenticate(user_name, user_password)
-
-            print "ThumbSignIn. Status of LDAP Authentication : %r" % (logged_in)
-
+            # When the username/password login fails, we need to reinitialize the ThumbSignIn data again before sending error response to UI
             if (not logged_in):
-                # Invoking the authenticate ThumbSignIn API via the Java SDK
-                authenticateResponseJsonStr = thumbsigninApiController.handleThumbSigninRequest("authenticate", ts_apiKey, ts_apiSecret)
-                print "ThumbSignIn. Value of authenticateResponseJsonStr is %s" % authenticateResponseJsonStr
-
-                authenticateResponseJsonObj = JSONObject(authenticateResponseJsonStr)
-                transactionId = authenticateResponseJsonObj.get("transactionId")
-                authenticationStatusRequest = "authStatus/" + transactionId
-                print "ThumbSignIn. Value of authenticationStatusRequest is %s" % authenticationStatusRequest
-
-                authorizationHeaderJsonStr = thumbsigninApiController.getAuthorizationHeaderJsonStr(authenticationStatusRequest, ts_apiKey, ts_apiSecret)
-                print "ThumbSignIn. Value of authorizationHeaderJsonStr is %s" % authorizationHeaderJsonStr
-                # {"authHeader":"HmacSHA256 Credential=XXX, SignedHeaders=accept;content-type;x-ts-date, Signature=XXX","XTsDate":"XXX"}
-
-                authorizationHeaderJsonObj = JSONObject(authorizationHeaderJsonStr)
-                authorizationHeader = authorizationHeaderJsonObj.get("authHeader")
-                xTsDate = authorizationHeaderJsonObj.get("XTsDate")
-                print "ThumbSignIn. Value of authorizationHeader is %s" % authorizationHeader
-                print "ThumbSignIn. Value of xTsDate is %s" % xTsDate
-
-                identity.setWorkingParameter("authenticateResponseJsonStr", authenticateResponseJsonStr)
-                identity.setWorkingParameter("authorizationHeader", authorizationHeader)
-                identity.setWorkingParameter("xTsDate", xTsDate)
+                self.initializeThumbSignInDataForWidget(thumbsigninApiController, identity)
                 return False
 
-            print "ThumbSignIn. Authenticate for step 1 successful"
+            print "ThumbSignIn. Authenticate successful for step %d" % step
             return True
 
         elif (step == 2):
             print "ThumbSignIn. Registration flow (step 2)"
+            self.verifyUserLoginFlow(identity)
 
-            if (identity.isSetWorkingParameter("userLoginFlow")):
-                userLoginFlow = identity.getWorkingParameter("userLoginFlow")
-                print "ThumbSignIn. Value of userLoginFlow is %s" % userLoginFlow
-            else:
-                identity.setWorkingParameter("userLoginFlow", "ThumbSignIn_Registration")
-                print "ThumbSignIn. Setting the value of userLoginFlow to %s" % identity.getWorkingParameter("userLoginFlow")
-
-            user = authenticationService.getAuthenticatedUser()
+            user = self.getAuthenticatedUserFromGluu(authenticationService)
             if user == None:
                 print "ThumbSignIn. Registration flow (step 2). Failed to determine user name"
                 return False
 
             user_name = user.getUserId()
-            print "ThumbSignIn. Registration flow (step 2). user_name: " + user_name
-
-            print "ThumbSignIn. Registration flow (step 2) successful"
+            print "ThumbSignIn. Registration flow (step 2) successful. user_name: " + user_name
             return True
+
         else:
             return False
+
+    def authenticateUserInAzureAD(self, identity, authenticationService):
+        credentials = identity.getCredentials()
+        user_name = credentials.getUsername()
+        user_password = credentials.getPassword()
+        print "ThumbSignIn. user_name: " + user_name
+        logged_in = False
+        if (StringHelper.isNotEmptyString(user_name) and StringHelper.isNotEmptyString(user_password)):
+
+            # Special condition to allow for Gluu admin login
+            if (StringHelper.equals(user_name, "admin")):
+                return self.authenticateUserInGluuLDAP(authenticationService, user_name, user_password)
+
+            # Authenticate user credentials with Azure AD non-interactively
+            azureAuthResponse = self.azureAuthConnector.authenticateUserInAzure(azure_tenant_id, user_name, user_password, azure_client_id, azure_client_secret)
+            print "ThumbSignIn. Value of azureAuthResponse is %s" % azureAuthResponse
+            azureAuthResponseJsonObj = JSONObject(azureAuthResponse)
+            if (azureAuthResponseJsonObj.has("upn")):
+                # Azure authentication has succeeded. User needs to be enrolled in Gluu LDAP
+                user = self.enrollAzureUserInGluuLDAP(azureAuthResponseJsonObj)
+                if (user == None):
+                    # User Enrollment in Gluu LDAP has failed
+                    logged_in = False
+                else:
+                    # Authenticating the user within Gluu
+                    userAuthenticatedInGluu = authenticationService.authenticate(user.getUserId())
+                    print "ThumbSignIn: Authentication status of the user enrolled in Gluu LDAP %r " % (userAuthenticatedInGluu)
+                    return userAuthenticatedInGluu
+            else:
+                # Azure authentication has failed.
+                logged_in = False
+        return logged_in
+
+    def enrollAzureUserInGluuLDAP(self, azureAuthResponseJsonObj):
+        # azuread_attributes_list  =  upn,given_name,family_name,upn
+        # gluuldap_attributes_list =  uid,givenName,sn,mail
+        # attributesMapping = ["upn:uid", "given_name:givenName", "family_name:sn", "upn:mail"]
+        userService = CdiUtil.bean(UserService)
+        azureUserPrincipalName = azureAuthResponseJsonObj.get("upn")
+        foundUser = self.findUserFromGluuLDAPByAttribute(userService, "mail", azureUserPrincipalName)
+        print "ThumbSignIn. Value of foundUser is %s" % foundUser
+        if (foundUser == None):
+            newUser = User()
+            self.populateUserObjWithAzureUserData(newUser, azureAuthResponseJsonObj)
+            try:
+                # Add azure user in Gluu LDAP
+                foundUser = userService.addUser(newUser, True)
+                foundUserId = foundUser.getUserId()
+                print("ThumbSignIn: Azure User added successfully in Gluu LDAP " + foundUserId)
+            except Exception, err:
+                print("ThumbSignIn: Error in adding azure user to Gluu LDAP:" + str(err))
+                return None
+        else:
+            self.populateUserObjWithAzureUserData(foundUser, azureAuthResponseJsonObj)
+            try:
+                # Update the user in Gluu LDAP with latest values from Azure AD
+                foundUser = userService.updateUser(foundUser)
+                foundUserId = foundUser.getUserId()
+                print("ThumbSignIn: Azure User updated successfully in Gluu LDAP " + foundUserId)
+            except Exception, err:
+                print("ThumbSignIn: Error in updating azure user to Gluu LDAP:" + str(err))
+                return None
+
+        return foundUser
+
+    def authenticateUserInGluuLDAP(self, authenticationService, user_name, user_password):
+        return authenticationService.authenticate(user_name, user_password)
+
+    def getAuthenticatedUserFromGluu(self, authenticationService):
+        return authenticationService.getAuthenticatedUser()
+
+    def findUserFromGluuLDAPByAttribute(self, userService, attributeName, attributeValue):
+        return userService.getUserByAttribute(attributeName, attributeValue)
+
+    def populateUserObjWithAzureUserData(self, user, azureAuthResponseJsonObj):
+        for attributesMappingEntry in self.attributesMapping.entrySet():
+            azureadAttribute = attributesMappingEntry.getKey()                              #upn
+            gluuldapAttribute = attributesMappingEntry.getValue()                           #uid
+            gluuldapAttributeValue = azureAuthResponseJsonObj.get(azureadAttribute)         #demo@ak1976hotmail.onmicrosoft.com
+            print "Value of gluuldapAttribute %s" % gluuldapAttribute
+            print "Value of gluuldapAttributeValue %s" % gluuldapAttributeValue
+            if ((gluuldapAttribute != None) & (gluuldapAttributeValue != "undefined")):
+                print gluuldapAttribute + gluuldapAttributeValue
+                user.setAttribute(gluuldapAttribute, gluuldapAttributeValue)
+        return None
+
+    def initializeThumbSignInDataForWidget(self, thumbsigninApiController, identity):
+        # Invoking the authenticate ThumbSignIn API via the Java SDK
+        authenticateResponseJsonStr = thumbsigninApiController.handleThumbSigninRequest("authenticate", ts_apiKey, ts_apiSecret)
+        print "ThumbSignIn. Value of authenticateResponseJsonStr is %s" % authenticateResponseJsonStr
+
+        authenticateResponseJsonObj = JSONObject(authenticateResponseJsonStr)
+        transactionId = authenticateResponseJsonObj.get("transactionId")
+        authenticationStatusRequest = "authStatus/" + transactionId
+        print "ThumbSignIn. Value of authenticationStatusRequest is %s" % authenticationStatusRequest
+
+        authorizationHeaderJsonStr = thumbsigninApiController.getAuthorizationHeaderJsonStr(authenticationStatusRequest, ts_apiKey, ts_apiSecret)
+        print "ThumbSignIn. Value of authorizationHeaderJsonStr is %s" % authorizationHeaderJsonStr
+        # {"authHeader":"HmacSHA256 Credential=XXX, SignedHeaders=accept;content-type;x-ts-date, Signature=XXX","XTsDate":"XXX"}
+
+        authorizationHeaderJsonObj = JSONObject(authorizationHeaderJsonStr)
+        authorizationHeader = authorizationHeaderJsonObj.get("authHeader")
+        xTsDate = authorizationHeaderJsonObj.get("XTsDate")
+        print "ThumbSignIn. Value of authorizationHeader is %s" % authorizationHeader
+        print "ThumbSignIn. Value of xTsDate is %s" % xTsDate
+
+        identity.setWorkingParameter("authenticateResponseJsonStr", authenticateResponseJsonStr)
+        identity.setWorkingParameter("authorizationHeader", authorizationHeader)
+        identity.setWorkingParameter("xTsDate", xTsDate)
+        return None
+
+    def getUserIdFromThumbSignIn(self, requestParameters, thumbsigninApiController):
+        transactionId = ServerUtil.getFirstValue(requestParameters, "transactionId")
+        print "ThumbSignIn. Value of transactionId is %s" % transactionId
+        getUserRequest = "getUser/" + transactionId
+        print "ThumbSignIn. Value of getUserRequest is %s" % getUserRequest
+
+        getUserResponseJsonStr = thumbsigninApiController.handleThumbSigninRequest(getUserRequest, ts_apiKey, ts_apiSecret)
+        print "ThumbSignIn. Value of getUserResponseJsonStr is %s" % getUserResponseJsonStr
+        getUserResponseJsonObj = JSONObject(getUserResponseJsonStr)
+        thumbSignIn_UserId = getUserResponseJsonObj.get("userId")
+        print "ThumbSignIn. Value of thumbSignIn_UserId is %s" % thumbSignIn_UserId
+        return thumbSignIn_UserId
+
+    def verifyUserLoginFlow(self, identity):
+        if (identity.isSetWorkingParameter("userLoginFlow")):
+            userLoginFlow = identity.getWorkingParameter("userLoginFlow")
+            print "ThumbSignIn. Value of userLoginFlow is %s" % userLoginFlow
+        else:
+            identity.setWorkingParameter("userLoginFlow", "ThumbSignIn_Registration")
+            print "ThumbSignIn. Setting the value of userLoginFlow to %s" % identity.getWorkingParameter("userLoginFlow")
 
     def getExtraParametersForStep(self, configurationAttributes, step):
         return None
